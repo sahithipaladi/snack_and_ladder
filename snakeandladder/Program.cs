194e44ad8e17588b930289a77ab0d1d7f2e9bef5
sahithi <sahithipari99@gmail.com>
2021-06-29 20:43:00 +0530

    [sahithi] player at 0

---
+++ snakeandladder/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace snakeandladder
+{
+    class Program
+    {
+        public const int STAST = 0;
+        public const int FINISH = 100;
+        static void Main(string[] args)
+        {
+       
+          
+            Console.WriteLine("Wellcome To Snake and Ladder game.");
+
+            int player_position = 0;
+            Console.WriteLine("player current position is " + player_position);
+        }
+    }
+}
