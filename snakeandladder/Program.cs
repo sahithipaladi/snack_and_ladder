a5e9ab6c7c8c5bf6a20ca709ed5a88b9d8529d0b
sahithi <sahithipari99@gmail.com>
2021-06-30 15:45:15 +0530

    [sahithi] using switch

--- snakeandladder/Program.cs
+++ snakeandladder/Program.cs
@@ -8,23 +8,54 @@ namespace snakeandladder
         //The Player starts from 0 and palys the game till the winning spot 100 is achieved.
         public const int START = 0;
         public const int FINISH = 100;
+        public const int NO_PLAY = 0, LADDER = 1, SNAKE = 2;
         public static Random random = new Random();
 
-        static int Roll_Dice()
+        public static int Roll_Dice()
         {
             // random value is given to palyer's dice
             return random.Next(1, 7);
+        }
+        public static int Player_Move_Option(int player_Roll_Dice)
+        {
+            // Player Option is gentrated..
+            int check_Player_Option = random.Next(0, 3);
+            int player_Move = 0;
+            switch (check_Player_Option)
+            {
+                case LADDER:
+                    player_Move = player_Roll_Dice;
+                    break;
+                case SNAKE:
+                    player_Move = -player_Roll_Dice;
+                    break;
+                default:
+                    break;
+            }
+            return player_Move;
         }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake and Ladder game.");
             // player initial position
-            int player_position = 0;
+            int player_position = 0, player_Next_Position;
             Console.WriteLine("Player current position is " + player_position);
 
             // player rolls the dice and gets the value
             int player_Roll_Dice = Roll_Dice();
             Console.WriteLine("Player Dice value is " + player_Roll_Dice);
+
+            // The Player then checks for a Option. They are No Play,Ladder or Snake.
+            int player_move = Player_Move_Option(player_Roll_Dice);
+            player_Next_Position = player_position + player_move;
+
+            if (player_Next_Position < START)
+                player_position = START;
+            else
+                player_position = player_Next_Position;
+            Console.WriteLine("Player current position After rolling the Dice is " + player_position);
+
+
         }
 
     }
