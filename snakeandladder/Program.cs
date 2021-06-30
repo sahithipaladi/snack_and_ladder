0979c8af14eea5c7e97cd0c17a090406d9d4bd59
sahithi <sahithipari99@gmail.com>
2021-06-30 16:18:39 +0530

    [sahithi] snakeandladder6

--- snakeandladder/Program.cs
+++ snakeandladder/Program.cs
@@ -40,14 +40,20 @@ namespace snakeandladder
             // player initial position
             int player_position = 0, player_Next_Position;
 
+            int dice_roll_count = 0;
+
             //Repeats till the Player reaches the winning position 100.
             while (player_position < FINISH)
             {
                 // player rolls the dice and gets the value
                 int player_Roll_Dice = Roll_Dice();
 
+                // number of times the dice was played is counted
+                dice_roll_count++;
+
                 // The Player then checks for a Option. They are No_Play,Ladder and Snake.
                 int player_move = Player_Move_Option(player_Roll_Dice);
+
                 if (player_position + player_move > FINISH)
                     player_Next_Position = player_position;
                 else
@@ -57,9 +63,13 @@ namespace snakeandladder
                     player_position = START;
                 else
                     player_position = player_Next_Position;
-            }
-            Console.WriteLine("Player current position After the game is " + player_position);
 
+                // Player current position After rolling the Dice
+                Console.WriteLine("Player current position After rolling the Dice is " + player_position);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Player current position After the game is " + player_position);
+            Console.WriteLine("Total Number of Dice Roll in the game is " + dice_roll_count);
 
 
 
